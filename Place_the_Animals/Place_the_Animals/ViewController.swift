ad0ca71d808555c2f55c82de2dea0406d00da293
Nathalia Trazzi <81270839+miucciaknows@users.noreply.github.com>
2024-03-27 11:24:21 -0300

    Atualizando projeto
    Atualizando a tela inicial, adicionando sons e efeitos para iniciar o minijogo

--- Place_the_Animals/Place_the_Animals/ViewController.swift
+++ Place_the_Animals/Place_the_Animals/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var hamster: UIImageView!
+    
+    var audioPlayer: AVAudioPlayer?
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+          let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+          hamster.addGestureRecognizer(tapGesture)
+          hamster.isUserInteractionEnabled = true
+          
+
+          if let soundURL = Bundle.main.url(forResource: "funny-spring-jump", withExtension: "mp3") {
+              do {
+                  audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                  audioPlayer?.prepareToPlay()
+              } catch {
+                  print("Erro ao carregar o arquivo de som: \(error.localizedDescription)")
+              }
+          } else {
+              print("Arquivo de som não encontrado")
+          }
+      }
+      
+      @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+          audioPlayer?.play()
+          
+          hamster.image = UIImage(named: "hamster")
+          
+          transitionToDogViewController()
+      }
+    
+    
+    func transitionToDogViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let dogViewController = storyboard.instantiateViewController(withIdentifier: "DogViewController") as? DogViewController {
+            print("DogViewController instanciado com sucesso.")
+            present(dogViewController, animated: true, completion: nil)
+        } else {
+            print("Erro ao instanciar DogViewController")
+        }
     }
-
-
-}
-
+    
+    
+    
+  }
